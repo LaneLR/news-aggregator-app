87afd567c520dcbd29145e64a2241ad6d8b878d0
LaneLR <laneleerichardson@gmail.com>
2025-06-16 15:06:37 -0500

    added check auth function

--- src/Provider.js
+++ src/Provider.js
@@ -3,7 +3,10 @@ import createSagaMiddleware from "redux-saga";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import rootSaga from "./app/sagas";
+import { useEffect } from "react";
 import LoggedInReducer from "./app/slices/manageLoggedIn";
+import { loginUser, logoutUser } from "./app/slices/manageLoggedIn"; 
+
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -18,6 +21,30 @@ const store = configureStore({
 sagaMiddleware.run(rootSaga);
 
 export default function Providers({ children }) {
+  const dispatch = store.dispatch; 
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      try {
+        const res = await fetch("/api/auth-status"); 
+        const data = await res.json();
+
+        if (data.isLoggedIn) {
+          //if server says user is logged in, update Redux state
+          dispatch(loginUser({ user: data.user, status: "active" }));
+        } else {
+          //if not logged in, ensure Redux state reflects it
+          dispatch(logoutUser());
+        }
+      } catch (err) {
+        console.error("Error checking auth status:", err);
+        dispatch(logoutUser()); //ensure user logged out if error
+      }
+    };
+
+    checkAuthStatus();
+  }, [dispatch]); 
+
   return (
     <>
       <Provider store={store}>{children}</Provider>
