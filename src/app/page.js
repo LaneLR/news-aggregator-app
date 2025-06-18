ac46ebfa7cabadd64de2053f5d331d38fec9a79a
LaneLR <laneleerichardson@gmail.com>
2025-06-18 16:46:16 -0500

    added Suspense to main page

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,15 @@
 import Header from "@/components/Header";
 import Link from "next/link";
 import NewsFeed from "@/components/NewsFeed";
-
+import { Suspense } from "react";
+import LoadingComponent from "@/components/Loading";
 
 export default function Home() {
   return (
     <>
-      <NewsFeed />
+      <Suspense fallback={<LoadingComponent />}>
+        <NewsFeed />
+      </Suspense>
     </>
   );
 }
