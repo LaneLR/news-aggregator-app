b2cc8b60a5a9b93c47dcdbb51adfb42bf1b06e55
LaneLR <laneleerichardson@gmail.com>
2025-06-18 16:46:34 -0500

    redesigned news card images

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -2,53 +2,97 @@
 import styled from "styled-components";
 import Image from "next/image";
 
-const CardWrapper = styled.div`
+const Wrapper = styled.div`
   width: 400px;
-  height: 300px;
-  display: flex;
   flex-flow: column nowrap;
   justify-content: center;
-  background-color: gray;
+  color: black;
+  background-color: white;
   margin: 10px 0;
-  border-radius: 5px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 20px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 10px;
 `;
 
-const ImageWrapper = styled.div`
+const CardWrapper = styled.div`
   width: 100%;
-  height: 100%;
-  overflow: hidden;
+  height: auto;
+  display: flex;
 `;
 
-const CardHeaderImage = styled.img`
+const DescriptionSection = styled.div`
   width: 100%;
-  height: 100%;
-  object-fit: stretch;
+  min-height: 70px;
+  display: flex;
+  flex-flow: column nowrap;
+  padding: 5px 7px 7px;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const TitleSection = styled.div`
   width: 100%;
-  height: 30%;
+  height: auto;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  font-size: 1rem;
+`;
+
+const AuthorSection = styled.div`
+  width: 100%;
+  height: auto;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  font-weight: 600;
+  font-size: 0.9rem;
 `;
 
 export default function NewsCard({ article }) {
-  if (!article.urlToImage) return null;
-  
+  const fallback = "/images/NoImage.png";
+  const index = article.title.lastIndexOf(" - ");
+  const cleanTitle =
+    index !== -1 ? article.title.substring(0, index) : article.title;
 
   return (
     <>
-      <CardWrapper>
-        <ImageWrapper>
+      <Wrapper>
+        <CardWrapper>
           <a href={article.url} target="_blank">
-            <Image 
-            src={article.urlToImage} 
-            width={400}
-            height={300}
-            alt={article.title} />
+            {article.urlToImage ? (
+              <Image
+                style={{
+                  borderTopLeftRadius: "15px",
+                  borderTopRightRadius: "10px",
+                }}
+                src={article.urlToImage}
+                width={400}
+                height={250}
+                alt={article.title}
+              />
+            ) : (
+              <Image
+                style={{
+                  borderTopLeftRadius: "15px",
+                  borderTopRightRadius: "10px",
+                  backgroundColor: "lightgray",
+                }}
+                src={fallback}
+                width={400}
+                height={300}
+                alt={article.title}
+              />
+            )}
           </a>
-        </ImageWrapper>
-
-        <TitleSection>{article.title}</TitleSection>
-      </CardWrapper>
+        </CardWrapper>{" "}
+        <DescriptionSection>
+          <TitleSection>{cleanTitle}</TitleSection>
+          <AuthorSection>{article.source.name}</AuthorSection>
+        </DescriptionSection>
+      </Wrapper>
     </>
   );
 }
