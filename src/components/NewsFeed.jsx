4323faa759a99b412be545a82ba8d5f91e2e6d7d
LaneLR <laneleerichardson@gmail.com>
2025-06-17 19:46:42 -0500

    added comonent to fetch news; included caching

---
+++ src/components/NewsFeed.jsx
@@ -0,0 +1,31 @@
+import NewsCard from "./NewsCard";
+
+async function fetchNews() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/news`, {
+    cache: 'force-cache',
+    next: { revalidate: 3600 },
+  });
+    if (!res.ok) {
+        throw new Error('Failed to fetch')
+    }
+    const data = await res.json();
+    console.log(data.articles)
+    return data.articles;
+}
+
+export default async function News() {
+    const articles = await fetchNews()
+
+  return (
+    <div style={{
+      display: 'flex',
+      flexFlow: 'row wrap',
+      justifyContent: 'space-evenly',
+      alignItems: 'center'
+    }}>
+      {articles.map((article, i) => (
+        <NewsCard key={i} article={article} />
+      ))}
+    </div>
+  );
+}
