f92f850362d99c008d86dc9a3e7fe4d7de7955af
LaneLR <laneleerichardson@gmail.com>
2025-07-04 22:43:42 -0500

    initial layout for user accounts page

---
+++ src/app/account/page.js
@@ -0,0 +1,19 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth-options.mjs";
+import { redirect } from "next/navigation";
+
+export default async function AccountPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/login");
+  }
+
+  return (
+    <div>
+      {/* name property doesn't exist in user, so commented out */}
+      {/* <h1>Welcome, {session.user.name}</h1> */} 
+      <p>Your email: {session.user.email}</p>
+    </div>
+  );
+}
