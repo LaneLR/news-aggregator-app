1754f2df6112e1617f22e1f5c6e135c82649f7c2
LaneLR <laneleerichardson@gmail.com>
2025-07-05 21:23:48 -0500

    use next-auth

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,7 +1,5 @@
 "use client";
-import { useDispatch } from "react-redux";
-import { logoutUser } from "@/app/slices/manageLoggedIn";
-import { useRouter } from "next/navigation";
+import { signOut } from "next-auth/react";
 import styled from "styled-components";
 
 const LogoutButton = styled.button`
@@ -19,27 +17,6 @@ const LogoutButton = styled.button`
 
 //create separate component for handling logout, then import into Header
 export default function LogoutComponent() {
-  const dispatch = useDispatch();
-  const router = useRouter();
 
-  const handleLogout = async () => {
-    try {
-      const res = await fetch("/api/logout", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-      });
-
-      if (res.ok) {
-        dispatch(logoutUser());
-        console.log("Logged out successfully.");
-        router.push("/login");
-      } else {
-        console.error("Logout failed on server side:", await res.json());
-      }
-    } catch (err) {
-      console.error("Client-side logout error:", err);
-    }
-  };
-
-  return <LogoutButton onClick={handleLogout}>Logout</LogoutButton>;
+  return <LogoutButton onClick={() => signOut({callbackUrl: '/login'}) }>Logout</LogoutButton>;
 }
