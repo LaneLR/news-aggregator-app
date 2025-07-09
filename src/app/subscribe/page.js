0a5bc26dd16d3a054f760ea819d86213d95300e5
LaneLR <laneleerichardson@gmail.com>
2025-07-08 21:00:42 -0500

    Subscription page

---
+++ src/app/subscribe/page.js
@@ -0,0 +1,13 @@
+import PaymentDetailsForm from "@/components/PaymentDetailsForm";
+
+
+export default function SubscribePage() {
+
+
+    return (
+        <>
+        <h1>Payment Information</h1>
+            <PaymentDetailsForm />
+        </>
+    )
+}
