ebd0026b7b1209a057de4c8a5e6401110477845d
LaneLR <laneleerichardson@gmail.com>
2025-07-08 20:58:10 -0500

    style changes

--- src/app/account/layout.js
+++ src/app/account/layout.js
@@ -3,12 +3,12 @@ import SideBarNav from "@/components/SideNavBar";
 export default function AccountLayout({ children }) {
   return (
     <>
-      <div style={{ display: "flex", height: "90vh", padding: "10px",}}>
+      <div style={{ display: "flex", height: "90vh", padding: "10px", width: '100%', }}>
         <div style={{ height: "100%" }}>
           <SideBarNav />
         </div>
-        <div style={{ display: "flex", height: "100%" }}>
-          <main style={{ flexGrow: "1" }}>{children}</main>
+        <div style={{ display: "flex", height: "100%", width: '100%' }}>
+          <main style={{ flexGrow: "1", width: '100%', }}>{children}</main>
         </div>
       </div>
     </>
