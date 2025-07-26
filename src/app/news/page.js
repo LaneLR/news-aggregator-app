3b29ae266143b4085fdd35120f56dd6f6e014889
LaneLR <laneleerichardson@gmail.com>
2025-07-26 16:45:07 -0500

    fixed newsfeed not displaying

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -8,14 +8,16 @@ import { Suspense } from "react";
 import News from "@/components/NewsFeed";
 
 export default async function Home() {
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-    if (!session) {
-        redirect("/");
-    }
+  if (!session) {
+    redirect("/");
+  }
+  return (
     <>
       <Suspense fallback={<LoadingComponent />}>
         <News />
       </Suspense>
     </>
+  );
 }
