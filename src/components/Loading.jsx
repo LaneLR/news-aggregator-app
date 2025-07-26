2875b8d91510e093e49b3c716f467fc13acdce23
LaneLR <laneleerichardson@gmail.com>
2025-07-26 16:44:32 -0500

    update background color

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,3 +1,3 @@
 export default function LoadingComponent() {
-  return <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '2rem'}}>Loading...</div>;
+  return <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '2rem', height: "100%", backgroundColor: "var(--light-white)"}}>Loading...</div>;
 }
