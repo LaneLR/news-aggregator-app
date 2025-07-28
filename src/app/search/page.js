97fd62b48d5b52b39d633c6d0ea482180d5d0bd3
LaneLR <laneleerichardson@gmail.com>
2025-07-27 20:54:55 -0500

    added session requirement for using search

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,7 +1,14 @@
 import SearchFeed from "@/components/SearchFeed";
-import { notFound } from "next/navigation";
+import { authOptions } from "@/lib/auth-options";
+import { getServerSession } from "next-auth";
+import { notFound, redirect } from "next/navigation";
 
-export default function SearchResultsPage({ searchParams }) {
+export default async function SearchResultsPage({ searchParams }) {
+    const session = await getServerSession(authOptions);
+  
+    if (!session) {
+      redirect("/");
+    }
   const query = searchParams?.query?.toLowerCase() || "";
 
   if (!query.trim()) return notFound();
