56fc6a330bf01a0562659b8317d2b982c3cc0b99
LaneLR <laneleerichardson@gmail.com>
2025-07-28 20:51:31 -0500

    inline syle changes

--- src/app/archives/[id]/page.js
+++ src/app/archives/[id]/page.js
@@ -2,7 +2,6 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth-options";
 import initializeDbAndModels from "@/lib/db";
 import { notFound } from "next/navigation";
-import NewsCard from "@/components/NewsCard"; // optional if you want to display styled cards
 import NewsGridWrapper from "@/components/NewsGridWrapper";
 import NewsCardThree from "@/components/NewsCardThree";
 
@@ -26,24 +25,74 @@ export default async function ArchiveDetailPage({ params }) {
 
   return (
     <div style={{ padding: "20px", color: "var(--dark-blue)" }}>
-      <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center',}}>
-        <h1>{archive.name} </h1> {" "}
-        <p><i>Links to some saved articles might break. This could be because the publisher has removed the article from their website or set the article to private.</i></p>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <h1>{archive.name}</h1>
         <div />
       </div>
       {articles.length === 0 ? (
-        <p>No saved articles yet.</p>
+        <div
+          style={{
+            display: "flex",
+            padding: '20px 0 0 0',
+            justifyContent: "center",
+            alignContent: "center",
+            width: "100%",
+            textAlign: "center",
+          }}
+        >
+          <p>No articles have been added to this Archive.</p>
+        </div>
+      ) : articles.length === 0 ? (
+        <div
+          style={{
+            display: "flex",
+            padding: '20px 0 0 0',
+            justifyContent: "center",
+            alignContent: "center",
+            width: "100%",
+            textAlign: "center",
+          }}
+        >
+          <p>No articles have been added to this Archive.</p>
+        </div>
       ) : (
-        <NewsGridWrapper>
-          {articles.map((article) => (
-            <NewsCardThree
-              key={article.id}
-              article={article}
-              archiveId={archive.id}
-              viewOnly={false}
-            />
-          ))}
-        </NewsGridWrapper>
+        <>
+          <NewsGridWrapper>
+            {articles.map((article) => (
+              <NewsCardThree
+                key={article.id}
+                article={article}
+                archiveId={archive.id}
+                viewOnly={false}
+              />
+            ))}
+          </NewsGridWrapper>
+          <div
+            style={{
+              padding: "0 20px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              position: "relative",
+              bottom: "0",
+              left: "0",
+            }}
+          >
+            <p>
+              <i>
+                Links to some saved articles might break. This could be caused by
+                the publisher removing the article from their website or setting
+                the article to private.
+              </i>
+            </p>
+          </div>
+        </>
       )}
     </div>
   );
