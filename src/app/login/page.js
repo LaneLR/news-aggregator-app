8963e762d59029e3440818acbfbce6907e76e1e8
LaneLR <laneleerichardson@gmail.com>
2025-08-06 19:36:15 -0500

    added session checking for login to prevent unauthorized access

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,8 +1,17 @@
 import LoadingComponent from "@/components/Loading";
 import LoginPage from "@/components/LoginForm";
+import { authOptions } from "@/lib/auth-options";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
-export default function Login() {
+export default async function Login() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/news");
+  }
+
   return (
     <>
       <Suspense fallback={<LoadingComponent />}>
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,8 +1,16 @@
 import LoadingComponent from "@/components/Loading";
 import RegisterPage from "@/components/RegisterPage";
+import { authOptions } from "@/lib/auth-options";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
-export default function Register() {
+export default async function Register() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/news");
+  }
   return (
     <>
       <Suspense fallback={<LoadingComponent />}>
