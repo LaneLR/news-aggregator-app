902ce310d18ffbac16b454b8ca61ed030767d8e8
LaneLR <laneleerichardson@gmail.com>
2025-08-07 20:58:17 -0500

    add account deletion  warning to profile page

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,5 +1,36 @@
 "use client";
+import Button from "./Button";
 import Divider from "./Divider";
+import styled from "styled-components";
+
+const WarningWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+  margin: 0 0 20px 0;
+`;
+
+const Underline = styled.div`
+  margin-top: 2px;
+  border-top: 2px solid #000;
+  width: 100%;
+`;
+
+const DeleteAccountWarning = styled.div`
+  background-color: rgba(255, 34, 34, 0.42);
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 16px 32px;
+  font-weight: 600;
+  border-radius: 6px;
+  margin: 0 0 20px 0;
+`;
 
 export default function ProfilePage({ session }) {
   const handleCancelDeletion = async () => {
@@ -35,6 +66,7 @@ export default function ProfilePage({ session }) {
       alert(
         "Account deletion requested. Your account will be deleted in 24 hours unless you cancel."
       );
+      location.reload();
     } catch (err) {
       console.error("Deletion request error:", err);
       alert("Something went wrong while requesting account deletion.");
@@ -43,12 +75,23 @@ export default function ProfilePage({ session }) {
 
   return (
     <>
-      {session.user.pendingDeletion !== null ? (
-        <div>
-          <p>Your account is scheduled to be deleted in 24 hours.</p>
-          <button onClick={handleCancelDeletion}>Cancel</button>
-        </div>
-      ) : ""}
+      {session.user.pendingDeletion !== false ? (
+        <WarningWrapper>
+          <DeleteAccountWarning>
+            <p>Your account is scheduled to be deleted at 11:59pm CT.</p>
+            <Underline />
+          </DeleteAccountWarning>
+          <Button
+            onClick={handleCancelDeletion}
+            bgColor={"#333333ff"}
+            clr={"var(--white)"}
+          >
+            Cancel Account Deletion
+          </Button>
+        </WarningWrapper>
+      ) : (
+        ""
+      )}
       <div
         style={{
           color: "var(--dark-blue)",
@@ -65,7 +108,7 @@ export default function ProfilePage({ session }) {
         <Divider />
         <p>Subscription stuff</p>
       </div>
-      <button onClick={handleRequestDeletion}>Request Account Deletion</button>
+      <Button onClick={handleRequestDeletion}>Request Account Deletion</Button>
     </>
   );
 }
