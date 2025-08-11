e0c4ae1c49883d3f9cd24b63b996cff9addf70b8
LaneLR <laneleerichardson@gmail.com>
2025-08-10 22:25:07 -0500

    styled verification pages

--- src/components/VerifyEmail.jsx
+++ src/components/VerifyEmail.jsx
@@ -1,9 +1,56 @@
+"use client";
+import Loading from "@/app/loading";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import styled from "styled-components";
+
+const TextWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 20px;
+  flex-direction: column;
+  gap: 20px;
+  height: 100%;
+  width: 100vw;
+`;
+
+const Text = styled.p`
+  color: var(--dark-blue);
+  font-weight: 600;
+  font-size: 1.1rem;
+  text-align: left;
+  width: 100%;
+`;
 
 export default function VerifyEmailComponent() {
-    return (
-        <>
-         <p>A verification email has been sent to your email.</p>
-         <p>You must confirm your email before continuing to the site.</p>
-        </>
-    )
+  const router = useRouter();
+  const { data: session, status, update } = useSession();
+
+  if (status === "loading") {
+    return <Loading />;
+  }
+
+  if (session && session.user.emailIsVerified) {
+    router.push("/news");
+  }
+
+  return (
+    <>
+      <TextWrapper>
+        <Image
+          style={{ margin: "20px 0 40px 0" }}
+          src={"/images/send-email.png"}
+          width={277}
+          height={112}
+          alt={"Image of an email being sent"}
+        />
+        <Text>A verification email has been sent to your email.</Text>
+        <Text>
+          Check your spam folder if you don&apos;t see the email in your inbox.
+        </Text>
+      </TextWrapper>
+    </>
+  );
 }
