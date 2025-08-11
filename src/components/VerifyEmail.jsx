1515d9c2520d44fd48c78461e15e4bf0cbc8b4a5
LaneLR <laneleerichardson@gmail.com>
2025-08-10 19:36:23 -0500

    created basic email verification components

---
+++ src/components/VerifyEmail.jsx
@@ -0,0 +1,9 @@
+
+export default function VerifyEmailComponent() {
+    return (
+        <>
+         <p>A verification email has been sent to your email.</p>
+         <p>You must confirm your email before continuing to the site.</p>
+        </>
+    )
+}
---
+++ src/components/VerifyEmailSuccess.jsx
@@ -0,0 +1,9 @@
+
+export default function VerifyEmailSuccessComponent() {
+    
+  return (
+    <>
+      <p>Your email has successfully been verified!</p>
+    </>
+  );
+}
