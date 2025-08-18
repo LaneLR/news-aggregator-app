c83dec1a344020dc2c4de1a94266c9cf79883c7f
LaneLR <laneleerichardson@gmail.com>
2025-08-17 21:22:50 -0500

    add Banner3 to HomePage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,6 +4,8 @@ import Button from "./Button";
 import Link from "next/link";
 import Image from "next/image";
 import TierCard from "./TierCard";
+import SubscriptionBanner from "./Banner2";
+import Banner3 from "./Banner3";
 
 const HomePageWrapper = styled.div`
   display: flex;
@@ -16,13 +18,42 @@ const HomePageWrapper = styled.div`
 
 const StyledHomePageSectionOne = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
   height: 100%;
   width: 100%;
-  background-color: var(--deep-blue);
-  padding: 65px 0;
+  background-color: var(--secondary-blue);
+  padding: 30px 0;
+  gap: 150px;
+  background-image: linear-gradient(
+    to bottom,
+    var(--dark-blue),
+    var(--secondary-blue)
+  );
+  @media (max-width: 1210px) {
+    gap: 10px;
+  }
+  @media (max-width: 1055px) {
+    padding: 24px 30px 80px 30px;
+    flex-direction: column;
+    gap: 31px;
+  }
+  @media (max-width: 440px) {
+    padding: 80px 30px;
+    flex-direction: column;
+    gap: 50px;
+  }
+`;
+
+const TransitionBox = styled.div`
+  background-image: linear-gradient(
+    to bottom,
+    var(--secondary-blue),
+    var(--white)
+  );
+  height: 53px;
+  width: 100%;
 `;
 
 const StyledHomePageSectionTwo = styled.div`
@@ -33,7 +64,18 @@ const StyledHomePageSectionTwo = styled.div`
   height: 100%;
   width: 100%;
   background-color: var(--white);
-  padding: 5% 0;
+  padding: 50px 0;
+`;
+
+const StyledHomePageSectionThree = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  background-color: var(--white);
+  padding: 50px 0;
 `;
 
 const SpaceFillerSection = styled.div`
@@ -54,6 +96,19 @@ const MainHeaderText = styled.h1`
   ${(props) => props.color && `color: ${props.color};`}
 `;
 
+const MainHeaderSubText = styled.p`
+  text-algin: justify;
+  font-size: 1.2rem;
+  text-align: center;
+  max-width: 600px;
+  ${(props) => props.color && `color: ${props.color};`}
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: left;
+  line-height: 1.3;
+`;
+
 const StyledParagraphLeft = styled.p`
   text-algin: justify;
   font-size: 1.1rem;
@@ -64,7 +119,6 @@ const StyledParagraphLeft = styled.p`
   justify-content: center;
   align-items: center;
   text-align: left;
-  padding: 0 10px;
 `;
 
 const StyledParagraphRight = styled.p`
@@ -127,36 +181,118 @@ const ExploreImageContainers = styled.div`
   margin-bottom: 10px;
 `;
 
+const FirstHeaderCTA = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: left;
+  width: auto;
+  height: 520px;
+  padding: 14px;
+  column-gap: 20px;
+  background-color: var(--white);
+  border-radius: 14px;
+
+  @media (max-width: 440px) {
+    width: 100%;
+    height: 376px;
+  }
+`;
+
+const HeaderButtonContainer = styled.div`
+  display: flex;
+  gap: 30px;
+  align-items: center;
+  @media (max-width: 440px) {
+    flex-direction: column;
+    width: 100%;
+    gap: 18px;
+  }
+`;
+
 export default function HomePage() {
   return (
     <HomePageWrapper>
       <StyledHomePageSectionOne>
-        <MainHeaderText color={"var(--light-white)"}>
-          Stay Informed.
-        </MainHeaderText>
-        <MainHeaderText color={"var(--light-white)"}>Your Way.</MainHeaderText>
-        <br />
-        <StyledParagraphCenter color={"var(--light-white)"}>
-          {/* Save and discover the latest news from around the web */}
-          Find and save all of your favorite news sources in one place
-        </StyledParagraphCenter>
-        <br />
-        {/* <input placeholder="Search articles"/> */}
-        <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
-          <Link href={"/register"}>
-            <Button bgColor={"var(--primary-blue)"} clr={"var(--white)"}>
-              Sign up
-            </Button>
-          </Link>
-          <Link href={"/login"}>
-            <Button bgColor={"var(--primary-blue)"} clr={"var(--white)"}>
-              Log in
-            </Button>
-          </Link>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <MainHeaderText color={"var(--light-white)"}>
+            Stay Informed.
+          </MainHeaderText>
+          <MainHeaderText color={"var(--light-white)"}>
+            Your Way.
+          </MainHeaderText>
+          <br />
+          <MainHeaderSubText color={"var(--light-white)"}>
+            {/* Save and discover the latest news from around the web */}
+            Find and save all of your favorite news sources, blog posts, science
+            journals, and market data and analysis all in one place.*
+          </MainHeaderSubText>
+          <br />
+          {/* <input placeholder="Search articles"/> */}
+          <HeaderButtonContainer>
+            <Link href={"/register"}>
+              <Button
+                bgColor={"var(--orange)"}
+                clr={"var(--white)"}
+                wide={"200px"}
+              >
+                Sign up
+              </Button>
+            </Link>
+            <Link href={"/login"}>
+              <Button
+                bgColor={"var(--orange)"}
+                clr={"var(--white)"}
+                wide={"200px"}
+              >
+                Log in
+              </Button>
+            </Link>
+          </HeaderButtonContainer>
         </div>
+        <FirstHeaderCTA>
+          <Banner3
+            title={"Free"}
+            features={[
+              "Access to hundreds of news sources and blog posts",
+              "3 Archives to save your favorite articles to",
+            ]}
+            cost={"$0 / mo"}
+          />
+          <Banner3
+            title={"Pro"}
+            features={[
+              "Access to hundreds of news sources and blog posts",
+              "Unlimited number of Archives to save your favorite articles",
+              "Access to journals and daily market data",
+            ]}
+            cost={"$8 / mo"}
+          />
+          <Banner3
+            title={"Pro Annual"}
+            features={[
+              "Access to hundreds of news sources and blog posts",
+              "Unlimited number of Archives to save your favorite articles",
+              "Access to journals and hourly market data",
+              "Customizable news feed",
+              "Priority support",
+              "Exclusive content and features",
+            ]}
+            cost={"$80 / yr"}
+          />
+        </FirstHeaderCTA>
       </StyledHomePageSectionOne>
+      <TransitionBox />
       <StyledHomePageSectionTwo>
-        <MainHeaderText color={"var(--deep-blue)"}>Discover the Latest News</MainHeaderText>
+        <MainHeaderText color={"var(--deep-blue)"}>
+          Discover the Latest News
+        </MainHeaderText>
         <StyledParagraphCenter>
           <br />
           Explore a wide range of articles from various sources, tailored to
@@ -289,6 +425,9 @@ export default function HomePage() {
           </ExploreImageContainers>
         </ImageContainer>
       </StyledHomePageSectionTwo>
+      <StyledHomePageSectionThree>
+        <SubscriptionBanner />
+      </StyledHomePageSectionThree>
       <SpaceFillerSection />
       {/* <TierCard title={"Pro"} /> */}
     </HomePageWrapper>
