a89e3b1d7f2894cd6f7fe068575055fe3a29671c
LaneLR <laneleerichardson@gmail.com>
2025-08-21 22:56:58 -0500

    create Journal page

---
+++ src/app/category/journal/page.js
@@ -0,0 +1,9 @@
+import CategoryPageComponent from "@/components/CategoryPage";
+
+export default function HealthNewsPage() {
+  return (
+    <>
+      <CategoryPageComponent category={"Journal"}/>
+    </>
+  );
+}
