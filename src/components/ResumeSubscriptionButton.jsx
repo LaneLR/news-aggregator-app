cb323f88ddc741ced07e8eb147705f70e06d1fb2
LaneLR <laneleerichardson@gmail.com>
2025-08-28 21:21:22 -0500

    add sessionData

--- src/components/ResumeSubscriptionButton.jsx
+++ src/components/ResumeSubscriptionButton.jsx
@@ -2,10 +2,16 @@
 
 import { useState } from "react";
 import Button from "./Button";
+import { useSession } from "next-auth/react";
 
-export default function ResumeSubscriptionButton({ updateSession }) {
-  const [isLoading, setIsLoading] = useState(false);
+export default function ResumeSubscriptionButton({
+  updateSession,
+  subscriptionEndDate,
+  sessionData,
+}) {
   const [error, setError] = useState(null);
+
+  const { data: session, status, update } = useSession({ data: sessionData });
 
   const handleResume = async () => {
     setError(null);
@@ -29,12 +35,33 @@ export default function ResumeSubscriptionButton({ updateSession }) {
   };
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <p style={{ marginBottom: "1rem" }}>
-        You have scheduled your subscription to cancel. You can resume it at any
-        time before the period ends.
-      </p>
-      <Button onClick={handleResume} bgColor="var(--deep-blue)" clr={"var(--white)"}>
+    <div
+      style={{
+        textAlign: "center",
+        color: "var(--dark-blue)",
+        padding: "10px 50px",
+      }}
+    >
+      <div style={{ marginBottom: "1rem" }}>
+        <p style={{padding: "0 0 5px 0"}}>
+          <i>
+            You have scheduled your subscription to cancel. You can resume it at
+            any time before the period ends.{" "}
+          </i>
+        </p>
+        <strong>
+          {" "}
+          Access ends on:{" "}
+          {new Date(
+            session?.user?.stripeSubscriptionEndsAt
+          ).toLocaleDateString()}
+        </strong>{" "}
+      </div>
+      <Button
+        onClick={handleResume}
+        bgColor="var(--deep-blue)"
+        clr={"var(--white)"}
+      >
         Resume Subscription
       </Button>
       {error && <p style={{ color: "red", marginTop: "10px" }}>{error}</p>}
