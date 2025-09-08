826bc82eca0ad748c6692ce3f74eed747f23ae35
LaneLR <laneleerichardson@gmail.com>
2025-09-07 22:39:24 -0500

    as timeout for redirect

--- src/app/verification/verify-email/page.js
+++ src/app/verification/verify-email/page.js
@@ -5,8 +5,10 @@ import { redirect } from "next/navigation";
 
 export default async function VerifyEmailPage() {
   const session = await getServerSession(authOptions);
-  if (session && session.user.emailIsVerified) {
-    return redirect("/");
+  if (session && session?.user?.emailIsVerified) {
+    setTimeout(() => {
+      redirect("/login");
+    }, 3000);
   }
 
   return (
